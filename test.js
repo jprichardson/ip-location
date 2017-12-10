a344f2f0a38b2c475f9c5104d3b973c0a5594510
Mike Dvorscak <mike.dvorscak@gmail.com>
2017-12-10 15:51:05 -0600

    Add missing invalid host/IP tests

--- test.js
+++ test.js
@@ -25,3 +25,38 @@ test('should fetch location with promise', function (t) {
     t.fail(err)
   })
 })
+
+test('should throw a meaningful error when an invalid ip is given', function (t) {
+  ipLocation('1.2.3')
+  .then(function (invalidRes) {
+    t.fail(invalidRes)
+  })
+  .catch(function (err) {
+    t.is(err, 'Invalid IP or hostname')
+    t.end()
+  })
+})
+
+test('shouldn\'t call the server with an invalid ip', function (t) {
+  var lastUrl
+  t.plan(1)
+  var oldHttpGet = ipLocation.httpGet
+  ipLocation.httpGet = function httpGetMock (url, callback) {
+    lastUrl = url
+  }
+  ipLocation('1.2.3')
+  t.is(lastUrl, undefined, 'should never post to the server')
+  ipLocation.httpGet = oldHttpGet // restore the original httpGet
+  t.end()
+})
+
+test('should allow real IPs', function (t) {
+  ipLocation('184.154.83.119')
+  .then(function (data) {
+    t.pass('valid IP')
+    t.end()
+  })
+  .catch(function (err) {
+    t.fail(err)
+  })
+})
